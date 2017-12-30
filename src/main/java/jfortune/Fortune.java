001785c179eda0d82f0d0a4ce0300d3ed2fd06a7
oboehm <o.boehm@optica.de>
2017-12-30 23:23:39 +0100

    config for uberjar added

--- src/main/java/jfortune/Fortune.java
+++ src/main/java/jfortune/Fortune.java
@@ -124,6 +124,7 @@ public final class Fortune {
      * @param args will be ignored.
      */
     public static void main(String[] args) {
+        System.setProperty("log4j.configurationFile", "log4j2-fortune.xml");
         Options options = getOptions();
         CommandLineParser parser = new DefaultParser();
         try {
